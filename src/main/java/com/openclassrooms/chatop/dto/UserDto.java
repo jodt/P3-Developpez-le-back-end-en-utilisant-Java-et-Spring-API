9e5cd8d30ff5983d21c22c13a4940fa537faf4aa
joel Dumortier <joel.dumortier@gmail.com>
2024-12-06 16:10:32 +0100

    create userDto

---
+++ src/main/java/com/openclassrooms/chatop/dto/UserDto.java
@@ -0,0 +1,24 @@
+package com.openclassrooms.chatop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+    Long id;
+
+    String email;
+
+    String name;
+
+    LocalDate createdAt;
+
+    LocalDate updatedAt;
+}
