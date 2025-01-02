4dd1f8707f0f8b61ad4777351ef46ee7d70dbf7d
joel Dumortier <joel.dumortier@gmail.com>
2025-01-02 15:50:38 +0100

    add javadoc

--- src/main/java/com/openclassrooms/chatop/configuration/WebConfig.java
+++ src/main/java/com/openclassrooms/chatop/configuration/WebConfig.java
@@ -11,6 +11,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Value("${file.upload-dir}")
     private String uploadDir;
 
+    /**
+     * Configures resource handlers to map requests for static resources.
+     * Maps requests matching "/uploads/owners/**" to files located in the designated upload directory.
+     *
+     * @param registry
+     */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/owners/**")
